12c70cca34ddee9614ca45fe55454c87685c3f67
MGP-28 <joaosilvapereira96@hotmail.com>
2022-10-04 19:55:08 +0100

    Created user list component to hold all the user cards

---
+++ src/components/UserList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="user-card-container">
+        <UserCard 
+            v-for="(user, idx) in users" :user="user" :key="idx"
+            @editing-user="editingUser(idx)"
+        ></UserCard>
+    </div>
+</template>
+
+<script>
+import UserCard from './UserCard.vue';
+    export default {
+    name: "UserList",
+    props: [
+        'users'
+    ],
+    methods: {
+        editingUser(idx){
+            this.$emit('editing-user', idx)
+        }
+    },
+    components: { UserCard }
+}
+</script>
+
+<style scoped>
+.user-card-container{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 50px;
+    max-width: 1200px;
+}
+</style>
