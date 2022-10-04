4bc082a1f4d7dcab9d4e9f83ff88339a0d762f69
MGP-28 <joaosilvapereira96@hotmail.com>
2022-10-04 23:25:37 +0100

    Implemented local storage saving and loading of users

--- src/components/ComponentContainer.vue
+++ src/components/ComponentContainer.vue
@@ -6,6 +6,7 @@
 </template>
 
 <script>
+import User from '../models/User';
 import UserForm from './UserForm.vue';
 import UserList from './UserList.vue';
     export default {
@@ -15,6 +16,9 @@ import UserList from './UserList.vue';
             users: [],
             userToEdit: null
         };
+    },
+    created() {
+        this.localStorageGetUsers()
     },
     methods: {
         newUser(user) {
@@ -29,10 +33,26 @@ import UserList from './UserList.vue';
         },
         editedUser(editedUser){
             this.users[this.userToEdit.idx] = editedUser
+        },
+        localStorageSetUsers(){
+            localStorage.setItem('users', JSON.stringify(this.users));
+        },
+        localStorageGetUsers(){
+            const users = JSON.parse(localStorage.getItem('users'));
+            users.forEach(user => {
+                this.users.push(
+                    new User(user.name, user.email, user.dob, user.isMale)
+                )
+            });
         }
     },
-    onUpdated(){
-        console.log(this.userToEdit)
+    watch: {
+        users:{
+            handler: function (val, oldVal){
+                this.localStorageSetUsers()
+            },
+            deep: true
+        }
     },
     components: { UserForm, UserList }
 }
