bff6d68d82728dd0fd15909b450a200aa168dc10
MGP-28 <joaosilvapereira96@hotmail.com>
2022-10-05 01:21:55 +0100

    Implemented image on User model

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,9 @@
 export default class User{
-    constructor(name = null, email = null, dob = null, isMale = null){
+    constructor(name = null, email = null, dob = null, isMale = null, image = null){
         this.name = name
         this.email = email
         this.dob = dob
         this.isMale = isMale
+        this.image = image
     }
 }
