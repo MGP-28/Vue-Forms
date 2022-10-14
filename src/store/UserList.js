937849ad1c5af1abba538eb2f677c7d4db0bdca0
MGP-28 <joaosilvapereira96@hotmail.com>
2022-10-14 18:54:04 +0100

    Created Pinia store for User List

---
+++ src/store/UserList.js
@@ -0,0 +1,41 @@
+import { defineStore } from 'pinia'
+
+import User from '../models/User'
+
+export const userList = defineStore('userList', {
+    state: () => ({ users: [] }),
+    getters: {
+        getUser: (idx) => JSON.parse(JSON.stringify(this.users[idx]))
+    },
+    actions: {
+        addUser(user){
+            this.users.push(user)
+            this.saveUsersToLC()
+        },
+        replaceUser(idx, newUser){
+            this.users[idx] = newUser
+            this.saveUsersToLC()
+        },
+        removeUser(idx){
+            //not used rn
+        },
+        saveUsersToLC(){
+            localStorage.setItem('users', JSON.stringify(this.users));
+        },
+        getUsersFromLC(){
+            const users = JSON.parse(localStorage.getItem('users'));
+            if(!users) return
+            users.forEach(user => {
+                this.users.push(
+                    new User(
+                        user.name, 
+                        user.email, 
+                        user.dob, 
+                        user.isMale,
+                        user.image
+                    )
+                )
+            });
+        }
+    }
+})
