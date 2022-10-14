41f7dcb9265bb189c6849a8b2c3810c2e81b1cc4
MGP-28 <joaosilvapereira96@hotmail.com>
2022-10-14 21:28:29 +0100

    Implemented Pinia into main component container

--- src/components/ComponentContainer.vue
+++ src/components/ComponentContainer.vue
@@ -1,66 +1,16 @@
 <template>
     <div class="content-grid">
-      <UserForm @new-user="newUser($event)" :userToEdit="userToEdit" @edited-user="editedUser($event)"></UserForm>
-      <UserList v-if="users.length > 0" :users="users" @editing-user="editingUser($event)"></UserList>
+      <UserForm></UserForm>
+      <UserList></UserList>
     </div>
 </template>
 
 <script>
-import User from '../models/User';
 import UserForm from './UserForm.vue';
 import UserList from './UserList.vue';
+
     export default {
     name: "ComponentContainer",
-    data() {
-        return {
-            users: [],
-            userToEdit: null
-        };
-    },
-    created() {
-        this.localStorageGetUsers()
-    },
-    methods: {
-        newUser(user) {
-            this.users.push(user);
-        },
-        editingUser(idx){
-            //JSON parsing the user in array to remove any references
-            this.userToEdit = {
-                user: JSON.parse(JSON.stringify(this.users[idx])), 
-                idx: idx
-            };
-        },
-        editedUser(editedUser){
-            this.users[this.userToEdit.idx] = editedUser
-        },
-        localStorageSetUsers(){
-            localStorage.setItem('users', JSON.stringify(this.users));
-        },
-        localStorageGetUsers(){
-            const users = JSON.parse(localStorage.getItem('users'));
-            if(!users) return
-            users.forEach(user => {
-                this.users.push(
-                    new User(
-                        user.name, 
-                        user.email, 
-                        user.dob, 
-                        user.isMale,
-                        user.image
-                    )
-                )
-            });
-        }
-    },
-    watch: {
-        users:{
-            handler: function (val, oldVal){
-                this.localStorageSetUsers()
-            },
-            deep: true
-        }
-    },
     components: { UserForm, UserList }
 }
 </script>
